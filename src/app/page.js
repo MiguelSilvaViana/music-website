f6c55b7c3e2240f0002be3b345049c5e94931677
Miguel Silva <miguelsilvavianaa@gmail.com>
2024-01-11 17:39:46 -0300

    albums

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 import Hero from '@/components/Hero';
 import Player from '@/components/Player';
+import Albums from '@/components/albums/Albums';
 import Events from '@/components/events/Events';
 import Image from 'next/image';
+
+/* json-server --watch --port 4000 ./_data/db.json */
 
 export default function Home() {
   return (
@@ -9,6 +12,7 @@ export default function Home() {
       <Hero />
       <Player />
       <Events />
+      <Albums />
       <div className='h-[4000px]'></div>
     </main>
   );
